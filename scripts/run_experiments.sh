7e71f1db72de986826a6b9b3ae634d7ac71de5b3
Elias Hanna <h.elias@hotmail.fr>
2022-05-29 12:05:12 +0100

    updated launch

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -5,9 +5,9 @@
 ##########Execute from data folder################
 ##################################################
 
-environments=(ant ball_in_cup redundant_arm fetch_pick_and_place)
-pred_error_plot_upper_limits=(20 5 1.25 2.5) # warning needs to be in same order as envs
-disagr_plot_upper_limits=(20 1 1.25 0.3) # warning needs to be in same order as envs
+environments=(ball_in_cup redundant_arm fetch_pick_and_place ant)
+pred_error_plot_upper_limits=(5 1.25 2.5 20) # warning needs to be in same order as envs
+disagr_plot_upper_limits=(1 1.25 0.3 20) # warning needs to be in same order as envs
 # environments=(ball_in_cup)
 # pred_error_plot_upper_limits=(5) # warning needs to be in same order as envs
 # disagr_plot_upper_limits=(1) # warning needs to be in same order as envs
@@ -15,36 +15,36 @@ reps=(0 1 2 3 4 5 6 7 8 9)
 episodes=(5 10 15 20)
 methods=(random-policies random-actions)
 
-# for env in "${environments[@]}"; do 
-#     mkdir ${env}_results
-#     cd ${env}_results
-#     for rep in "${reps[@]}"; do
-# 	    mkdir $rep
-# 	    cd $rep
-# 	    for method in "${methods[@]}"; do
-# 	        for ep in "${episodes[@]}"; do
-# 		        python ../../../scripts/launch_exp.py --init-method $method --init-episodes $ep --environment $env --dump-path .
-# 	        done
-# 	    done
-# 	    cd ..
-#     done
-#     cd ..	
-# done
+for env in "${environments[@]}"; do 
+    mkdir ${env}_results
+    cd ${env}_results
+    for rep in "${reps[@]}"; do
+	    mkdir $rep
+	    cd $rep
+	    for method in "${methods[@]}"; do
+	        for ep in "${episodes[@]}"; do
+		        python ../../../scripts/launch_exp.py --init-method $method --init-episodes $ep --environment $env --dump-path .
+	        done
+	    done
+	    cd ..
+    done
+    cd ..	
+done
 
 # wait
 
 ## Plot means over replications
 
-# for env in "${environments[@]}"; do
-#     cd ${env}_results
-#     echo "Processing following folder"; pwd
-# 	for method in "${methods[@]}"; do
-# 	    for ep in "${episodes[@]}"; do
-#             python ../../scripts/plot_after_run.py --init-method $method --init-episodes $ep --environment $env --dump-path .
-#         done
-#     done
-#     cd ..
-# done
+for env in "${environments[@]}"; do
+    cd ${env}_results
+    echo "Processing following folder"; pwd
+	for method in "${methods[@]}"; do
+	    for ep in "${episodes[@]}"; do
+            python ../../scripts/plot_after_run.py --init-method $method --init-episodes $ep --environment $env --dump-path .
+        done
+    done
+    cd ..
+done
 
 # wait
 
