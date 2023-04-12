7371da44dd9f4b563cb09a1a3937076f4c401761
Elias Hanna <h.elias@hotmail.fr>
2023-04-12 16:10:45 +0200

    added rewards with inc_rew params arg

--- model_init_study/initializers/initializer.py
+++ model_init_study/initializers/initializer.py
@@ -31,7 +31,11 @@ class Initializer:
         self._act_dim = params['action_dim']
         self.nb_thread = cpu_count() - 1 or 1
         self.policies = None
-
+        if 'inc_rew' in params:
+            self.inc_rew = params['inc_rew']
+        else:
+            self.inc_rew = False
+            
     @abstractmethod
     def _get_action(self, obs, t):
         """
@@ -51,6 +55,7 @@ class Initializer:
         # traj = []
         # actions = []
         cum_rew = 0
+        reward = 0
         ## WARNING: need to get previous obs
         for t in range(self._env_max_h):
             if self._is_goal_env:
@@ -58,7 +63,10 @@ class Initializer:
             action = self._get_action(idx, obs, t)
             action[action>self._action_max] = self._action_max
             action[action<self._action_min] = self._action_min
-            transitions.append((action, obs))
+            if self.inc_rew:
+                transitions.append((action, obs, reward))
+            else:
+                transitions.append((action, obs))
             obs, reward, done, info = env.step(action)
             cum_rew += reward
             if done:
@@ -67,7 +75,10 @@ class Initializer:
             # actions.append(action)
         if self._is_goal_env:
             obs = obs['observation']
-        transitions.append((None, obs))
+        if self.inc_rew:
+            transitions.append((None, obs, reward))
+        else:
+            transitions.append((None, obs))
         ## Return transitions
         return transitions
     
@@ -76,4 +87,5 @@ class Initializer:
         pool = Pool(processes=self.nb_thread)
         ret = pool.starmap(self._eval_policy,
                            zip(range(self._n_init_episodes + self._n_test_episodes)))
+        pool.close()
         return ret
